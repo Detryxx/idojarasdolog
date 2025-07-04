e6bfffe3c7ba11426a77def5f80d3929c0a31dbb
Detryxx <kelebeni2010@gmail.com>
2025-07-04 11:01:04 +0200

    asfdsadsadí

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import requests
-import json
-response_API = requests.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2e0846b09ae1c196567ee72580eecec0')
-print(response_API.status_code)
+import ast
+
+
+def get_weather(city = str):
+    response_API = requests.get(f'http://api.weatherapi.com/v1/current.json?key=b2c05218b2004702b9784017250407&q={city}&aqi=no')
+
+    temp = ast.literal_eval(response_API.text).get("current").get("temp_c")
+    condition = ast.literal_eval(response_API.text).get("current").get("condition").get("text")
+    is_it_good = response_API.status_code
+    return city, temp, condition, is_it_good
+
+
+
+if __name__ == "__main__":
+    print(get_weather("London"))
