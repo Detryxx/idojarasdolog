469c2aeb8e40f220cb5a81f6cc83c061a0e72d2e
Detryxx <kelebeni2010@gmail.com>
2025-07-04 11:33:08 +0200

    Same

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
 """
-    requests -- for requesting data fro the api
-    ast -- for literal_eval, to turn the returnded str into a dict
+requests -- for requesting data fro the api
+ast -- for literal_eval, to turn the returnded str into a dict
 """
+
 import requests
 import ast
 
+
 def get_weather(city=str):
     """
-        Keyword Arguments:
-        city -- the user defined city to wich to get the weather from
+    Keyword Arguments:
+    city -- the user defined city to wich to get the weather from
 
-        Fetches the temperature and the condition from the city given
-        trough the weatherapi api and checks for errors.
+    Fetches the temperature and the condition from the city given
+    trough the weatherapi api and checks for errors.
     """
     response_API = requests.get(
-        #fetches the data with of the given city
+        # fetches the data with of the given city
         f"http://api.weatherapi.com/v1/current.json?key=b2c05218b2004702b9784017250407&q={city}&aqi=no"
     )
 
-    try:    #tries to fetch the temperature in Celsius and the condition
+    try:  # tries to fetch the temperature in Celsius and the condition
         temp = ast.literal_eval(response_API.text).get("current").get("temp_c")
         condition = (
             ast.literal_eval(response_API.text)
@@ -26,10 +28,10 @@ def get_weather(city=str):
             .get("condition")
             .get("text")
         )
-    except: #if it cant fetch the above stated data, an error must have orruced so it returns the rror code
+    except:  # if it cant fetch the above stated data, an error must have orruced so it returns the rror code
         error_code = ast.literal_eval(response_API.text).get("error").get("code")
         return error_code
-    else:   #if good, returns the requested data
+    else:  # if good, returns the requested data
         return city, temp, condition
 
 
