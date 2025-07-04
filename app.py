96a58c8ec6d3b132b0df880ee3ea7088b193ceda
Detryxx <kelebeni2010@gmail.com>
2025-07-04 11:18:42 +0200

    asfasasfsaffs

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ import ast
 def get_weather(city = str):
     response_API = requests.get(f'http://api.weatherapi.com/v1/current.json?key=b2c05218b2004702b9784017250407&q={city}&aqi=no')
 
-    temp = ast.literal_eval(response_API.text).get("current").get("temp_c")
-    condition = ast.literal_eval(response_API.text).get("current").get("condition").get("text")
-    is_it_good = response_API.status_code
-    return city, temp, condition, is_it_good
-
+    try:
+        temp = ast.literal_eval(response_API.text).get("current").get("temp_c")
+        condition = ast.literal_eval(response_API.text).get("current").get("condition").get("text")
+        is_it_good = response_API.status_code
+    except:
+        error_code = ast.literal_eval(response_API.text).get("error").get("code")
+        return error_code
+    else:
+        return city, temp, condition, is_it_good
 
 
 if __name__ == "__main__":
